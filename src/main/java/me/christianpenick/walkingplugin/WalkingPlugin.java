23937b3d400c60e771b4665282c2431a6eb4d852
ChristianCompiles <christiancompiles@gmail.com>
2023-07-26 00:02:26 -0700

    Beginning event listener.

--- src/main/java/me/christianpenick/walkingplugin/WalkingPlugin.java
+++ src/main/java/me/christianpenick/walkingplugin/WalkingPlugin.java
@@ -1,17 +1,44 @@
 package me.christianpenick.walkingplugin;
 
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class WalkingPlugin extends JavaPlugin {
+public final class WalkingPlugin extends JavaPlugin implements Listener
+{
+
+    public WalkingPlugin(){
+
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Parkour plugin started.");
+        this.getServer().getPluginManager().registerEvents(this, this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Parkour plugin ended.");
+    }
+
+
+    @EventHandler
+    public void EntityHoldBlockEvent(Entity event) {
+
+        if(event.getEntityType() == EntityType.PLAYER) {
+            Player player = (Player)event.getEntity();
+            if (player.getHand() != EquipmentSlot.HAND)
+            Block userBlock = (Player)event.getBlock();
+        }
+
     }
 }
