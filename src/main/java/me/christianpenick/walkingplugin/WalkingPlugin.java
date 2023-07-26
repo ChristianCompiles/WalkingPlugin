c3961d7c098b9f199514581b7c223215b1f3950e
ChristianCompiles <christiancompiles@gmail.com>
2023-07-26 14:21:34 -0700

    Functionality is plausible (1).

--- src/main/java/me/christianpenick/walkingplugin/WalkingPlugin.java
+++ src/main/java/me/christianpenick/walkingplugin/WalkingPlugin.java
@@ -1,12 +1,18 @@
 package me.christianpenick.walkingplugin;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.meta.tags.ItemTagType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WalkingPlugin extends JavaPlugin implements Listener
@@ -19,7 +25,7 @@ public final class WalkingPlugin extends JavaPlugin implements Listener
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Parkour plugin started.");
+        System.out.println("Walking plugin started.");
         this.getServer().getPluginManager().registerEvents(this, this);
 
     }
@@ -27,18 +33,36 @@ public final class WalkingPlugin extends JavaPlugin implements Listener
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("Parkour plugin ended.");
+        System.out.println("Walking plugin ended.");
     }
-
 
     @EventHandler
-    public void EntityHoldBlockEvent(Entity event) {
+    public void EntityHoldBlockEvent(PlayerInteractEvent event) {
+        // Player object
+        Player player = (Player)event.getPlayer();
 
-        if(event.getEntityType() == EntityType.PLAYER) {
-            Player player = (Player)event.getEntity();
-            if (player.getHand() != EquipmentSlot.HAND)
-            Block userBlock = (Player)event.getBlock();
+        // radius of blocks around player
+        int radius = 2;
+
+        Material handItemType = player.getInventory().getItemInMainHand().getType();
+        Block blockUnderPlayer = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+
+        if (handItemType.isBlock() && !blockUnderPlayer.isEmpty()) // player.getItemInHand() != null <-- might be necessary check
+        {
+            Location playerLoc = player.getLocation();
+            double pX = playerLoc.getX();
+            double pY = playerLoc.getY();
+            double pZ = playerLoc.getZ();
+
+            for (int x = -(radius); x <= radius; x ++)
+            {
+                for (int y = -(radius); y <= radius; y ++)
+                {
+                    Block b = player.getWorld().getBlockAt((int)pX+x, (int)pY+y, (int)pZ);
+                    b.setType(handItemType);
+                }
+            }
         }
-
     }
 }
+
