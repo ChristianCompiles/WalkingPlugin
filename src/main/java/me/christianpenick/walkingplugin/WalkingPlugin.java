a505d9b45a6427678dc49f51ca3fff9d8d0a8e24
ChristianCompiles <christiancompiles@gmail.com>
2023-07-26 14:23:12 -0700

    Functionality is plausible (1).

--- src/main/java/me/christianpenick/walkingplugin/WalkingPlugin.java
+++ src/main/java/me/christianpenick/walkingplugin/WalkingPlugin.java
@@ -4,15 +4,10 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.meta.tags.ItemTagType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WalkingPlugin extends JavaPlugin implements Listener
@@ -39,7 +34,7 @@ public final class WalkingPlugin extends JavaPlugin implements Listener
     @EventHandler
     public void EntityHoldBlockEvent(PlayerInteractEvent event) {
         // Player object
-        Player player = (Player)event.getPlayer();
+        Player player = event.getPlayer();
 
         // radius of blocks around player
         int radius = 2;
